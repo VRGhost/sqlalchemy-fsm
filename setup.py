3e1a16459155f11d9fc9bb0cea06d7f25f369688
Actions <github+actions@gmail.com>
2022-08-08 12:04:00 +0000

    Bump version: 2.0.12 → 2.0.13

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "Topic :: Database",
     ],
     keywords="sqlalchemy finite state machine fsm",
-    version="2.0.12",
+    version="2.0.13",
     url="https://github.com/VRGhost/sqlalchemy-fsm",
     install_requires=[
         "SQLAlchemy>=1.0.0",
